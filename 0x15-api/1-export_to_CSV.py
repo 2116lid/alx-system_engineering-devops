ddb51b9c9e0388e2f08adef117bffdd348142fe2
Noah Alemayehu <noahlem21@gmail.com>
2024-02-19 07:24:01 -0800

    task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format"""
+import sys
+import requests
+import csv
+
+
+if __name__ == "__main__":
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    user = [i for i in users if i.get('id') == int(sys.argv[1])][0]
+    all_task = [i for i in data if i.get('userId') == int(sys.argv[1])]
+    with open(sys.argv[1] + '.csv', 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        for task in all_task:
+            writer.writerow([str(sys.argv[1]), user.get('username'),
+                             str(task.get('completed')), task.get('title')])
+        f.close()
