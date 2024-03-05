00e7fb0fed07d8fe584084f672dac2b9cfd7397e
Noah Alemayehu <noahlem21@gmail.com>
2024-03-05 11:30:25 -0800

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API
+and returns the number of subscribers.
+"""
+import requests
+
+
+Red_URL = 'https://www.reddit.com'
+
+
+
+def number_of_subscribers(subreddit):
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(Red_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
