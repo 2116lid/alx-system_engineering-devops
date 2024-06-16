12ac9856bc24fb81c0b04dd2605db0e4b21f2835
2116lid <135040968+2116lid@users.noreply.github.com>
2024-06-16 09:37:14 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# A puppet file to fix apache.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,1 +0,0 @@
-This file contains web stack debugging three.
